02010a818deee6adaac6005312e48565e79515cb
HarrisonTCodes <htillbackup@gmail.com>
2024-10-06 11:20:48 +0100

    Added initial search params to trains page

--- app/trains/page.tsx
+++ app/trains/page.tsx
@@ -1,7 +1,8 @@
-export default function TrainsPage() {
+export default function TrainsPage({ searchParams }: { searchParams: {from: string, to: string} }) {
   return (
     <main>
       <h1>Trains</h1>
+      <p>From {searchParams.from} to {searchParams.to}</p>
     </main>
   )
 }
