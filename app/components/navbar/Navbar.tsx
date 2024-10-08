9c9cb9af255bdb2c109249895ada4481ac8b901c
HarrisonTCodes <htillbackup@gmail.com>
2024-10-08 19:20:13 +0100

    Added hamburger menu open/close logic

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import NavbarButton from "./NavbarButton";
 import { FaBookmark, FaUser, FaTrain, FaBars } from "react-icons/fa";
 
@@ -8,22 +10,32 @@ const buttonData = [
 ] 
 
 export default function Navbar() {
+  const [hamburgerMenuOpen, setHamburgerMenuOpen] = useState(false)
+
   return (
     <nav className="w-full h-14 px-4 flex justify-between items-center z-10 bg-blue-800">
+      {/* App Name */}
       <h1 className="text-2xl text-white font-medium">Live Trains UK</h1>
+      {/* Buttons */}
       <section className="flex gap-8">
         {buttonData.map((data) => (
-          <NavbarButton href={data.href}>{data.icon} {data.label}</NavbarButton>
+          <NavbarButton href={data.href} key={`${data.label} nav option`}>{data.icon} {data.label}</NavbarButton>
         ))}
-        <button className="md:hidden">
+        {/* Hamburger Button */}
+        <button className="md:hidden" onClick={() => setHamburgerMenuOpen(!hamburgerMenuOpen)}>
           <FaBars size={32} color="ffffff" className="text-white" />
         </button>
       </section>
-      <div className="flex md:hidden flex-col cursor-pointer absolute w-48 top-16 right-2 border-2 border-stone-300 bg-white rounded-xl divide-y-2 divide-stone-300">
-        {buttonData.map((data) => (
-          <a href={data.href} className="px-2 py-2 text-lg text-stone-600 font-medium">{data.label}</a>
-        ))}
-      </div>
+      {/* Hamburger Menu */}
+      {hamburgerMenuOpen &&
+        <div className="flex md:hidden flex-col cursor-pointer absolute w-48 top-16 right-2 border-2 border-stone-300 bg-white rounded-xl divide-y-2 divide-stone-300">
+          {buttonData.map((data) => (
+            <a href={data.href} className="px-2 py-2 text-lg text-stone-600 font-medium" key={`${data.label} menu option`}>
+              {data.label}
+            </a>
+          ))}
+        </div>
+      }
     </nav>
   )
 }
