709316b3a2503d22fe2f85b26aa3c2d9f3a6d8d3
HarrisonTCodes <htillbackup@gmail.com>
2024-10-11 20:10:46 +0100

    Added state to search bar

--- app/components/search/Search.tsx
+++ app/components/search/Search.tsx
@@ -1,8 +1,29 @@
-export default function Search() {
+import { ChangeEvent } from 'react';
+
+export default function Search({
+  label,
+  value,
+  setValue,
+}: {
+  label: string;
+  value: string;
+  setValue: (value: string) => void;
+}) {
+  const onChange = (ev: ChangeEvent<HTMLInputElement>) => {
+    setValue(ev.target.value);
+    // TODO make request to API and get matching station options here
+  };
+
   return (
-    <input
-      type="search"
-      className="w-[80vw] max-w-96 rounded-lg border-2 border-gray-400 bg-gray-200 p-2 text-xl focus:border-blue-800 focus:outline-none md:w-[40vw]"
-    />
+    <div className="flex w-[80vw] max-w-96 flex-col md:w-[40vw]">
+      <input
+        type="search"
+        className="w-full rounded-lg border-2 border-gray-400 bg-gray-100 p-2 text-xl focus:border-blue-800 focus:outline-none"
+        placeholder={label}
+        value={value}
+        onChange={onChange}
+      />
+      {/* TODO menu with options matching search here */}
+    </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
+'use client';
+import { useState } from 'react';
 import Search from './components/search/Search';
 
 export default function LiveTrainsPage() {
+  const [from, setFrom] = useState<string>('');
+  const [to, setTo] = useState<string>('');
+
   return (
-    <main className="flex flex-col items-center gap-6 py-8">
+    <main className="flex flex-col items-center gap-6 py-6">
       <h1 className="text-3xl font-medium text-blue-900">Get Live Departures</h1>
       <section className="flex flex-col gap-4 md:flex-row">
-        <Search />
-        <Search />
+        <Search label="From" value={from} setValue={setFrom} />
+        <Search label="To" value={to} setValue={setTo} />
       </section>
     </main>
   );
