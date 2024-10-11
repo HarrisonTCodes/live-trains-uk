1118309ec2f9be837a580b398e043bfec8ab8b04
HarrisonTCodes <htillbackup@gmail.com>
2024-10-11 18:00:39 +0100

    Added initial search bar component and home page UI

---
+++ app/components/search/Search.tsx
@@ -0,0 +1,8 @@
+export default function Search() {
+  return (
+    <input
+      type="search"
+      className="w-[80vw] max-w-96 rounded-lg border-2 border-gray-400 bg-gray-200 p-2 text-xl focus:border-blue-800 focus:outline-none md:w-[40vw]"
+    />
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import Search from './components/search/Search';
+
 export default function LiveTrainsPage() {
   return (
-    <main>
-      <h1>Live Trains</h1>
+    <main className="flex flex-col items-center gap-6 py-8">
+      <h1 className="text-3xl font-medium text-blue-900">Get Live Departures</h1>
+      <section className="flex flex-col gap-4 md:flex-row">
+        <Search />
+        <Search />
+      </section>
     </main>
   );
 }
