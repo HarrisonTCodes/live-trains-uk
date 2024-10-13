d247e73ce23b42ba7dffe862541fb0f891387909
HarrisonTCodes <htillbackup@gmail.com>
2024-10-13 17:06:53 +0100

    Added headings to train page

--- app/trains/page.tsx
+++ app/trains/page.tsx
@@ -18,7 +18,13 @@ export default function TrainsPage({
   }, []);
 
   return (
-    <main>
+    <main className="flex flex-col items-center gap-6 py-8">
+      <section className="text-center">
+        <h1 className="text-3xl font-medium text-blue-900">Live Departures</h1>
+        <h2 className="text-lg font-medium text-blue-900">
+          {searchParams.from} to {searchParams.to}
+        </h2>
+      </section>
       <p>{JSON.stringify(services)}</p>
     </main>
   );
