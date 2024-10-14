a73f03b9483608e7fa337560b1017f3482053e97
HarrisonTCodes <htillbackup@gmail.com>
2024-10-14 18:25:34 +0100

    Added functionality to switch button (trains page)

--- app/trains/page.tsx
+++ app/trains/page.tsx
@@ -4,6 +4,7 @@ import { Service } from '../interfaces';
 import Button from '../components/button/Button';
 import { FaArrowRightArrowLeft, FaArrowRotateRight } from 'react-icons/fa6';
 import TrainInfo from '../components/train-info/TrainInfo';
+import { useRouter } from 'next/navigation';
 
 export default function TrainsPage({
   searchParams,
@@ -13,6 +14,7 @@ export default function TrainsPage({
   const [services, setServices] = useState<Service[]>([]);
   const [fromCrs, setFromCrs] = useState<string>('');
   const [toCrs, setToCrs] = useState<string>('');
+  const router = useRouter();
 
   const getData = () => {
     setServices([]);
@@ -27,28 +29,35 @@ export default function TrainsPage({
       });
   };
 
+  const switchStations = () => {
+    router.push(`/trains?from=${searchParams.to}&to=${searchParams.from}`);
+  };
+
   useEffect(() => {
     getData();
-  }, []);
+  }, [searchParams]);
 
   return (
     <main className="flex flex-col items-center gap-6 py-8">
+      {/* Headings */}
       <section className="text-center">
         <h1 className="text-3xl font-medium text-blue-900">Live Departures</h1>
         <h2 className="text-lg font-medium text-blue-900">
           {searchParams.from} to {searchParams.to}
         </h2>
       </section>
+      {/* Buttons */}
       <section className="flex gap-2">
         <Button onClick={getData}>
           <FaArrowRotateRight size={24} color="ffffff" className="text-white" />
           Refresh
         </Button>
-        <Button>
+        <Button onClick={switchStations}>
           <FaArrowRightArrowLeft size={24} color="ffffff" className="text-white" />
           Switch
         </Button>
       </section>
+      {/* Trains */}
       <section className="flex w-full flex-col items-center gap-4">
         {services.map((service) => (
           <TrainInfo service={service} from={fromCrs} to={toCrs} />
