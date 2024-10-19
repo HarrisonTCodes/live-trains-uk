80e940019ed3096455782917626d82590199f8c9
HarrisonTCodes <htillbackup@gmail.com>
2024-10-19 11:24:20 +0100

    Added initial styling to train page

--- app/train/[id]/page.tsx
+++ app/train/[id]/page.tsx
@@ -22,18 +22,38 @@ export default function TrainPage() {
 
   return (
     <main className="flex flex-col items-center gap-6 py-8">
+      {/* Heading */}
       <h1
-        className="flex items-center gap-2 text-3xl font-medium text-blue-900"
+        className="flex cursor-pointer items-center gap-2 text-3xl font-medium text-blue-900"
         onClick={() => router.back()}
       >
         <FaArrowLeft size={24} />
-        Return
+        Return to Departures
       </h1>
-      {callingPoints.map((callingPoint: CallingPoint) => (
-        <p>
-          {callingPoint.station} {callingPoint.departureTime} {callingPoint.estimatedDepartureTime}
-        </p>
-      ))}
+      {/* Calling points */}
+      <div className="relative flex w-[90vw] max-w-[500px] flex-col rounded-xl bg-gray-200 px-[14.5px]">
+        {/* Line */}
+        <div className="absolute left-5 h-full w-1 rounded-full bg-blue-900" />
+        {/* Points */}
+        <section>
+          {callingPoints.map((callingPoint: CallingPoint) => (
+            <section className="flex items-center gap-2">
+              {/* Circle */}
+              <div className="z-10 my-10 h-[16px] w-[16px] rounded-full bg-blue-700" />
+              {/* Details */}
+              <section className="w-full">
+                <p>{callingPoint.station}</p>
+                <p>
+                  {callingPoint.departureTime}
+                  {callingPoint.estimatedDepartureTime
+                    ? ` (${callingPoint.estimatedDepartureTime})`
+                    : ''}
+                </p>
+              </section>
+            </section>
+          ))}
+        </section>
+      </div>
     </main>
   );
 }
