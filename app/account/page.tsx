c88b2dea816dba10d0508232076c03ec6d7575ae
HarrisonTCodes <htillbackup@gmail.com>
2024-10-21 19:41:51 +0100

    Initial auth setup

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,7 +1,12 @@
-export default function AccountPage() {
+import { options } from '../api/auth/[...nextauth]/options';
+import { getServerSession } from 'next-auth';
+
+export default async function AccountPage() {
+  const session = await getServerSession(options);
+
   return (
     <main>
-      <h1>Account</h1>
+      <h1>{session?.user?.email ?? 'No user'}</h1>
     </main>
   );
 }
