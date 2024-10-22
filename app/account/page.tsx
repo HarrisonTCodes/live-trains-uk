94085eb5e85ede270a963b3a0eacc41f11efa472
HarrisonTCodes <htillbackup@gmail.com>
2024-10-22 11:15:37 +0100

    Added prisma and account creation

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -5,8 +5,9 @@ export default async function AccountPage() {
   const session = await getServerSession(options);
 
   return (
-    <main>
-      <h1>{session?.user?.email ?? 'No user'}</h1>
+    <main className="flex flex-col items-center gap-6 py-8">
+      <h1 className="text-3xl font-medium text-blue-900">Account</h1>
+      <p className="text-lg">Logged in as {session?.user?.email}</p>
     </main>
   );
 }
--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,3 +1,4 @@
+import prisma from '@/app/utils/prisma';
 import type { NextAuthOptions } from 'next-auth';
 import Google from 'next-auth/providers/google';
 
@@ -8,4 +9,25 @@ export const options: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      if (!user.email) throw new Error('No user');
+
+      // Create user if first time logging in, update name if there has been a change
+      await prisma.user.upsert({
+        where: {
+          email: user.email,
+        },
+        create: {
+          email: user.email,
+          name: user.name,
+        },
+        update: {
+          name: user.name,
+        },
+      });
+
+      return true;
+    },
+  },
 };
