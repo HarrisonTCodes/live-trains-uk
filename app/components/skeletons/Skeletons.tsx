b21524bb7a0414f56cc2c1fa707f1efff8e70443
HarrisonTCodes <htillbackup@gmail.com>
2024-10-23 10:09:08 +0100

    Generalised skeleton component and added loading/error handling to journeys

--- app/components/skeletons/Skeletons.tsx
+++ app/components/skeletons/Skeletons.tsx
@@ -1,5 +1,5 @@
-export default function TrainInfoSkeletons() {
-  return Array.from({ length: 5 }).map((_, index: number) => (
+export default function Skeletons({ count = 5 }: { count?: number }) {
+  return Array.from({ length: count }).map((_, index: number) => (
     <div
       key={`skeleton${index}`}
       className="flex h-24 w-11/12 max-w-[500px] animate-pulse divide-x-2 rounded-xl bg-gray-300"
--- app/my-journeys/page.tsx
+++ app/my-journeys/page.tsx
@@ -3,16 +3,28 @@ import { useEffect, useState } from 'react';
 import { Journey } from '../interfaces';
 import JourneyInfo from '../components/journey-info/JourneyInfo';
 import Button from '../components/button/Button';
-import { FaPlus } from 'react-icons/fa6';
+import { FaPlus, FaTriangleExclamation } from 'react-icons/fa6';
 import Link from 'next/link';
+import Skeletons from '../components/skeletons/Skeletons';
 
 export default function JourneysPage() {
   const [journeys, setJourneys] = useState<Journey[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
 
   useEffect(() => {
+    setLoading(true);
     fetch('/api/journeys')
       .then((response) => response.json())
-      .then((response) => setJourneys(response));
+      .then((response) => {
+        setJourneys(response);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+        setError(true);
+      });
   }, []);
 
   return (
@@ -25,14 +37,25 @@ export default function JourneysPage() {
         </Button>
       </Link>
       <section className="flex w-full flex-col items-center gap-4">
-        {journeys.map((journey: Journey) => (
-          <JourneyInfo
-            key={`journey ${journey.name}`}
-            journey={journey}
-            journeys={journeys}
-            setJourneys={setJourneys}
-          />
-        ))}
+        {loading ? (
+          <Skeletons />
+        ) : error ? (
+          <h2 className="flex items-center gap-2 text-2xl font-medium text-red-700">
+            <FaTriangleExclamation size={24} color="ff0000" className="text-red-700" /> There was an
+            error
+          </h2>
+        ) : journeys.length === 0 ? (
+          <h2>No journeys</h2>
+        ) : (
+          journeys.map((journey: Journey) => (
+            <JourneyInfo
+              key={`journey ${journey.name}`}
+              journey={journey}
+              journeys={journeys}
+              setJourneys={setJourneys}
+            />
+          ))
+        )}
       </section>
     </main>
   );
