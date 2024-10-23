4e7d645d3a48ab2de171fb504206ffbfa2b4c0ff
HarrisonTCodes <htillbackup@gmail.com>
2024-10-23 12:00:15 +0100

    Added sign out button to account page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,5 +1,8 @@
 import { options } from '../api/auth/[...nextauth]/options';
 import { getServerSession } from 'next-auth';
+import Button from '../components/button/Button';
+import { FaArrowRightToBracket } from 'react-icons/fa6';
+import Link from 'next/link';
 
 export default async function AccountPage() {
   const session = await getServerSession(options);
@@ -7,7 +10,13 @@ export default async function AccountPage() {
   return (
     <main className="flex flex-col items-center gap-6 py-8">
       <h1 className="text-3xl font-medium text-blue-900">Account</h1>
-      <p className="text-lg">Logged in as {session?.user?.email}</p>
+      <p className="px-2 text-center text-xl">Logged in as {session?.user?.email}</p>
+      <Link href={'/api/auth/signout'}>
+        <Button>
+          <FaArrowRightToBracket size={24} color="ffffff" className="text-white" />
+          Sign Out
+        </Button>
+      </Link>
     </main>
   );
 }
