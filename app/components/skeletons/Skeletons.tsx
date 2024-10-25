3d2542a7c0db5929257e3c9607b8d3e201faa66d
HarrisonTCodes <htillbackup@gmail.com>
2024-10-25 11:14:52 +0100

    Removed redundant classes from skeletons

--- app/components/skeletons/Skeletons.tsx
+++ app/components/skeletons/Skeletons.tsx
@@ -2,7 +2,7 @@ export default function Skeletons({ count = 5 }: { count?: number }) {
   return Array.from({ length: count }).map((_, index: number) => (
     <div
       key={`skeleton${index}`}
-      className="flex h-24 w-11/12 max-w-[500px] animate-pulse divide-x-2 rounded-xl bg-gray-300"
+      className="h-24 w-11/12 max-w-[500px] animate-pulse rounded-xl bg-gray-300"
     />
   ));
 }
