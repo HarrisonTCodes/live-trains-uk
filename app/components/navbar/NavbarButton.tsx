a90573889b8e2c711e7ca9505a981a47b7660491
HarrisonTCodes <htillbackup@gmail.com>
2024-10-26 13:24:56 +0100

    Fixed navbar button click not triggering on border

--- app/components/navbar/NavbarButton.tsx
+++ app/components/navbar/NavbarButton.tsx
@@ -1,15 +1,18 @@
+import Link from 'next/link';
+
 export default function NavbarButton({
   children,
   href,
 }: {
   children: React.ReactNode;
-  href?: string;
+  href: string;
 }) {
   return (
-    <button className="hidden rounded-xl p-2 font-medium text-white hover:bg-blue-600 active:bg-blue-500 md:block">
-      <a href={href} className="flex items-center justify-center gap-2">
-        {children}
-      </a>
-    </button>
+    <Link
+      className="hidden items-center justify-center gap-2 rounded-xl p-2 font-medium text-white hover:bg-blue-600 active:bg-blue-500 md:flex"
+      href={href}
+    >
+      {children}
+    </Link>
   );
 }
