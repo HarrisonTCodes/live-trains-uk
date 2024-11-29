1bf5545b8bbe4bc5059e5ffe124e904d999e29aa
HarrisonTCodes <htillbackup@gmail.com>
2024-11-29 18:09:25 +0000

    Readded average duration calculation

--- app/trains/[from]/[to]/page.tsx
+++ app/trains/[from]/[to]/page.tsx
@@ -12,6 +12,11 @@ export default async function TrainsPage({ params }: { params: { from: string; t
   const parsedTo = params.to.replaceAll('%2B', ' ').replaceAll('%20', ' ').toLowerCase();
   const services = await getServices(parsedFrom, parsedTo);
   const lastUpdated = services.time ? ` (Last Updated at ${services.time})` : '';
+  const averageDuration =
+    services.services.reduce(
+      (accumulator: number, service: Service) => accumulator + service.duration,
+      0,
+    ) / services.services.length;
 
   return (
     <main className="flex flex-col items-center gap-6 py-8">
@@ -38,7 +43,7 @@ export default async function TrainsPage({ params }: { params: { from: string; t
               fromCrs={services.fromCrs}
               toStation={parsedTo}
               toCrs={services.toCrs}
-              averageDuration={0}
+              averageDuration={averageDuration}
             />
           ))
         ) : (
