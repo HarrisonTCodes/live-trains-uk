396d837b377d4ba552e3d4e2e88ff518bc01f7a4
HarrisonTCodes <htillbackup@gmail.com>
2024-11-30 16:21:34 +0000

    Improved loading response for service details

--- app/train/[id]/loading.tsx
+++ app/train/[id]/loading.tsx
@@ -3,8 +3,10 @@ import PageHeading from '@/app/components/page-heading/PageHeading';
 export default function Loading() {
   return (
     <main className="flex flex-col items-center gap-6 py-8">
-      <PageHeading heading="Service details" />
-      <h2 className="text-xl font-medium text-gray-500">Loading service details...</h2>
+      <PageHeading heading="Service details" subHeading="Loading details..." />
+      <div className="relative h-[100vh] w-[90vw] max-w-[500px] rounded-xl bg-gray-200">
+        <h2 className="py-4 text-center text-xl text-gray-400">Loading service details...</h2>
+      </div>
     </main>
   );
 }
