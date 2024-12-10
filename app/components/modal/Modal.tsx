ac3f89713df99586c311473cf8fc56d17d8a4319
HarrisonTCodes <htillbackup@gmail.com>
2024-12-10 18:04:38 +0000

    Changed modal from absolute to fixed positioning

--- app/components/modal/Modal.tsx
+++ app/components/modal/Modal.tsx
@@ -17,7 +17,7 @@ export default function Modal({
   confirmIcon?: React.ReactNode;
 }) {
   return (
-    <div className="absolute left-[50%] top-[50%] z-50 flex min-h-[250px] w-[90%] max-w-[450px] translate-x-[-50%] translate-y-[-50%] flex-col items-center justify-between rounded-xl border-2 border-gray-300 bg-white px-2 py-4 shadow-lg">
+    <div className="fixed left-[50%] top-[50%] z-50 flex min-h-[250px] w-[90%] max-w-[450px] translate-x-[-50%] translate-y-[-50%] flex-col items-center justify-between rounded-xl border-2 border-gray-300 bg-white px-2 py-4 shadow-lg">
       <h2 className="text-xl font-medium text-blue-900">{title}</h2>
       {children}
       <section className="flex w-full justify-evenly">
