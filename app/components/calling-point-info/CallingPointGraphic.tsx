e47c36103790c3ff54f782ea7ca9f45747abba69
HarrisonTCodes <htillbackup@gmail.com>
2024-12-20 12:50:34 +0000

    Moved calling point graphic into component and cleaned up

---
+++ app/components/calling-point-info/CallingPointGraphic.tsx
@@ -0,0 +1,9 @@
+export default function CallingPointGraphic() {
+  return (
+    <svg width="25" height="100" xmlns="http://www.w3.org/2000/svg">
+      <rect x="9" width="6" height="40" fill="#1e40af" />
+      <circle cx="12" cy="50" y="30" r="8" stroke="#1e40af" strokeWidth="4" fill="none" />
+      <rect x="9" y="60" width="6" height="40" fill="#1e40af" />
+    </svg>
+  );
+}
--- app/components/calling-point-info/CallingPointInfo.tsx
+++ app/components/calling-point-info/CallingPointInfo.tsx
@@ -1,16 +1,13 @@
 import { CallingPoint } from '@/app/interfaces';
 import formatEstimated from '@/app/utils/formatEstimated';
 import { AiOutlineExclamationCircle } from 'react-icons/ai';
+import CallingPointGraphic from './CallingPointGraphic';
 
 export default function CallingPointInfo({ callingPoint }: { callingPoint: CallingPoint }) {
   return (
     <section className="flex items-center gap-2">
       {/* Graphic */}
-      <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
-        <rect x="9" width="6" height="40" fill="#1e40af" />
-        <circle cx="12" cy="50" y="30" r="8" stroke="#1e40af" stroke-width="4" fill="none" />
-        <rect x="9" y="60" width="6" height="40" fill="#1e40af" />
-      </svg>
+      <CallingPointGraphic />
       {/* Details */}
       <section className="w-full">
         <p className={`text-lg ${callingPoint.focus ? 'font-bold' : 'font-medium'}`}>
