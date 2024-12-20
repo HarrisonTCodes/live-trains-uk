752cf0d16adde97693e03c54265fa98477db8f34
HarrisonTCodes <htillbackup@gmail.com>
2024-12-20 18:20:51 +0000

    Updated calling points to show clearly when service has already departed

--- app/components/calling-point-info/CallingPointInfo.tsx
+++ app/components/calling-point-info/CallingPointInfo.tsx
@@ -18,12 +18,20 @@ export default function CallingPointInfo({
       <CallingPointGraphic isFirstPoint={isFirstPoint} isLastPoint={isLastPoint} />
       {/* Details */}
       <section className="w-full pt-6">
+        {/* Station and platform */}
         <p className={`text-lg ${callingPoint.focus ? 'font-bold' : 'font-medium'}`}>
           {callingPoint.station}{' '}
           {callingPoint.platform ? `(Platform ${callingPoint.platform})` : ''}
         </p>
+        {/* Times */}
         <p className="flex gap-2">
-          {callingPoint.departureTime}
+          {/* Departure time */}
+          {callingPoint.estimatedDepartureTime ? (
+            callingPoint.departureTime
+          ) : (
+            <span className="text-gray-600">{callingPoint.departureTime} (Departed)</span>
+          )}
+          {/* Estimated time */}
           {callingPoint.estimatedDepartureTime && (
             <span
               className={`flex items-center gap-1 font-medium ${callingPoint.estimatedDepartureTime === 'On time' ? 'text-green-700' : 'text-red-800'}`}
