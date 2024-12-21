0a23e1af1c24a87999c321dc7434c10faad55671
HarrisonTCodes <htillbackup@gmail.com>
2024-12-21 18:11:26 +0000

    Changed calling point graphic lines from rects to lines

--- app/components/calling-point-info/CallingPointGraphic.tsx
+++ app/components/calling-point-info/CallingPointGraphic.tsx
@@ -7,9 +7,9 @@ export default function CallingPointGraphic({
 }) {
   return (
     <svg width="25" height="110" xmlns="http://www.w3.org/2000/svg">
-      {!isFirstPoint && <rect x="9" width="6" height="45" fill="#1e40af" />}
+      {!isFirstPoint && <line x1="12" y1="0" x2="12" y2="49" strokeWidth="6" stroke="#1e40af" />}
       <circle cx="12" cy="55" r="8" stroke="#1e40af" strokeWidth="4" fill="none" />
-      {!isLastPoint && <rect x="9" y="65" width="6" height="45" fill="#1e40af" />}
+      {!isLastPoint && <line x1="12" y1="61" x2="12" y2="110" strokeWidth="6" stroke="#1e40af" />}
     </svg>
   );
 }
