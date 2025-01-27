ae3a9690ffa5f65bed8c022048e22cb9519bc1ef
HarrisonTCodes <htillbackup@gmail.com>
2025-01-27 20:03:13 +0000

    Updated details error page to match new style

--- app/train/[id]/error.tsx
+++ app/train/[id]/error.tsx
@@ -1,12 +1,11 @@
 'use client';
 import Notice from '@/app/components/notice/Notice';
-import PageHeading from '@/app/components/page-heading/PageHeading';
 import { FaTriangleExclamation } from 'react-icons/fa6';
 
 export default function Error() {
   return (
     <main className="flex flex-col items-center gap-6 py-8">
-      <PageHeading heading="Service details" />
+      <h1 className="pb-2 text-center text-2xl font-bold text-blue-900">Service Details</h1>
       <Notice
         notice="Error"
         description="There was an error trying to retrieve the service. This service may no longer exist"
