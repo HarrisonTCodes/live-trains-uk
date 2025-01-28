9103aa9d89a00e48c0d9365eea6c9f53bbcf0a62
HarrisonTCodes <htillbackup@gmail.com>
2025-01-28 19:32:18 +0000

    Clean up notice component

--- app/components/notice/Notice.tsx
+++ app/components/notice/Notice.tsx
@@ -18,14 +18,14 @@ export default function Notice({
     <div
       className={`min-h-24 w-[90vw] max-w-[700px] gap-2 rounded-lg border p-2 ${!status && 'border-stone-300 bg-white'} ${status === 'success' && 'border-green-700 bg-green-50 text-green-900'} ${(status === 'fail' || status === 'alert') && 'border-red-700 bg-red-50 text-red-900'}`}
     >
-      <h2 className={`flex items-center gap-1 text-xl font-medium`}>
-        {!status && <FaCircleInfo className="text-stone-600" />}
+      <h2 className="flex items-center gap-1 text-xl font-medium">
+        {!status && <FaCircleInfo className="text-blue-800" />}
         {status === 'success' && <FaCircleCheck />}
         {status === 'fail' && <FaTriangleExclamation />}
         {status === 'alert' && <FaCircleExclamation />}
         {notice}
       </h2>
-      <p className="">{description}</p>
+      <p>{description}</p>
     </div>
   );
 }
