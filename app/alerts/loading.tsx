1a50ed2a3a218db3b7326ec4ac2986aabad54fe0
HarrisonTCodes <htillbackup@gmail.com>
2025-01-29 18:10:00 +0000

    Fixed spacing on alerts page

--- app/alerts/loading.tsx
+++ app/alerts/loading.tsx
@@ -2,9 +2,11 @@ import Skeletons from '../components/skeletons/Skeletons';
 
 export default function Loading() {
   return (
-    <main className="flex flex-col items-center gap-6 py-8">
+    <main className="flex flex-col items-center gap-1 py-8">
       <h1 className="pb-2 text-center text-2xl font-bold text-blue-900">Alerts and Disruptions</h1>
-      <Skeletons height="h-24" />
+      <div className="flex flex-col items-center gap-6">
+        <Skeletons height="h-24" />
+      </div>
     </main>
   );
 }
--- app/alerts/page.tsx
+++ app/alerts/page.tsx
@@ -9,33 +9,38 @@ export default async function AlertsPage() {
     .catch((err) => console.error(err));
 
   return (
-    <main className="flex flex-col items-center gap-6 py-8">
-      <h1 className="pb-2 text-center text-2xl font-bold text-blue-900">Alerts and Disruptions</h1>
-      {alerts ? (
-        alerts.length > 0 ? (
-          alerts.map((alert: { name: string; summary: string }, index: number) => (
+    <main className="flex flex-col items-center gap-4 py-8">
+      {/* Heading */}
+      <h1 className="text-center text-2xl font-bold text-blue-900">Alerts and Disruptions</h1>
+
+      {/* Alerts */}
+      <div className="flex flex-col items-center gap-6">
+        {alerts ? (
+          alerts.length > 0 ? (
+            alerts.map((alert: { name: string; summary: string }, index: number) => (
+              <Notice
+                key={`alert-${index}`}
+                notice={toTitleCase(alert.name.replaceAll('-', ' '))}
+                description={alert.summary}
+                status="alert"
+              />
+            ))
+          ) : (
             <Notice
-              key={`alert-${index}`}
-              notice={toTitleCase(alert.name.replaceAll('-', ' '))}
-              description={alert.summary}
-              status="alert"
+              notice="No alerts"
+              description="No alerts or disruptions at the moment."
+              status="success"
             />
-          ))
+          )
         ) : (
+          // If there was an error
           <Notice
-            notice="No alerts"
-            description="No alerts or disruptions at the moment."
-            status="success"
+            notice="Error"
+            description="There was an error trying to retrieve alerts and disruptions."
+            status="fail"
           />
-        )
-      ) : (
-        // If there was an error
-        <Notice
-          notice="Error"
-          description="There was an error trying to retrieve alerts and disruptions."
-          status="fail"
-        />
-      )}
+        )}
+      </div>
     </main>
   );
 }
