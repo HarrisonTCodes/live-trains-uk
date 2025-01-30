38c2435ac071a08ea2f89281352b3f830240ec4f
HarrisonTCodes <htillbackup@gmail.com>
2025-01-30 20:08:59 +0000

    Added greyed text for disabled secondary button

--- app/components/button/Button.tsx
+++ app/components/button/Button.tsx
@@ -22,7 +22,7 @@ export default function Button({
 
   return (
     <button
-      className={`flex items-center justify-center gap-2 rounded-lg p-2 font-medium transition disabled:cursor-not-allowed ${secondary ? 'border border-stone-300 enabled:hover:bg-stone-100 disabled:bg-stone-200' : 'border border-blue-800 bg-blue-800 text-white enabled:hover:border-blue-600 enabled:hover:bg-blue-600'} ${width}`}
+      className={`flex items-center justify-center gap-2 rounded-lg p-2 font-medium transition disabled:cursor-not-allowed ${secondary ? 'border border-stone-300 enabled:hover:bg-stone-100 disabled:bg-stone-200 disabled:text-stone-400' : 'border border-blue-800 bg-blue-800 text-white enabled:hover:border-blue-600 enabled:hover:bg-blue-600'} ${width}`}
       // If button has no onclick and is a back button, onclick navigate to previous page
       onClick={onClick ?? (back ? () => router.back() : () => {})}
       type={submit ? 'submit' : 'button'}
