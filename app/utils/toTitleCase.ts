a89b697f44361d7641424a2e5536e5de60efd97f
HarrisonTCodes <htillbackup@gmail.com>
2025-01-31 17:27:49 +0000

    Updated title case function to work with kebab-case and ignore special words

--- app/utils/toTitleCase.ts
+++ app/utils/toTitleCase.ts
@@ -1,6 +1,26 @@
+// Special words that appear untitled (first letter lowercase) in station names
+const doNotTitleCase = [
+  'en',
+  'le',
+  'on',
+  'under',
+  'in',
+  'upon',
+  'y',
+  'by',
+  'super',
+  'over',
+  'the',
+  'bach',
+  'at',
+  'gron',
+  'for',
+];
+
 export default function toTitleCase(str: string) {
-  return str.replace(
-    /\w\S*/g,
-    (text) => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase(),
+  return str.replace(/\b\w+/g, (text) =>
+    doNotTitleCase.includes(text)
+      ? text
+      : text.charAt(0).toUpperCase() + text.substring(1).toLowerCase(),
   );
 }
