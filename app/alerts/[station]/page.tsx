4c6c19de1b443298f365fff3dd33e078a4ec558e
HarrisonTCodes <htillbackup@gmail.com>
2025-02-24 20:22:06 +0000

    Added alerts section component and loading alerts handling

--- app/alerts/[station]/page.tsx
+++ app/alerts/[station]/page.tsx
@@ -1,11 +1,14 @@
+import AlertsSection from '@/app/components/alert/AlertsSection';
 import Button from '@/app/components/button/Button';
 import HeadingWidget from '@/app/components/heading-widget/HeadingWidget';
 import Notice from '@/app/components/notice/Notice';
+import Skeletons from '@/app/components/skeletons/Skeletons';
 import { Alert } from '@/app/interfaces';
 import getAlerts from '@/app/utils/getAlerts';
 import getTime from '@/app/utils/getTime';
 import toTitleCase from '@/app/utils/toTitleCase';
 import { ArrowLeftIcon } from 'lucide-react';
+import { Suspense } from 'react';
 
 export default async function StationAlertsPage(props: { params: Promise<{ station: string }> }) {
   const params = await props.params;
@@ -15,18 +18,29 @@ export default async function StationAlertsPage(props: { params: Promise<{ stati
 
   return (
     <main className="flex flex-col items-center gap-6 py-8">
-      <h1 className="pb-2 text-center text-2xl font-bold text-blue-900">Alerts and Disruptions</h1>
-      <HeadingWidget
-        text={`Alerts and disruptions for ${toTitleCase(parsedStation)}`}
-        tag={`Last updated at ${now}`}
-      >
-        <Button width="w-full md:w-56" back>
-          <ArrowLeftIcon /> Back to alerts
-        </Button>
-      </HeadingWidget>
-      {alerts.map((alert: Alert, index: number) => (
-        <Notice key={index} notice="Alert" description={alert.message} status="alert" />
-      ))}
+      <section className="flex flex-col items-center gap-1">
+        {/* Heading */}
+        <h1 className="pb-2 text-center text-2xl font-bold text-blue-900">
+          Alerts and Disruptions
+        </h1>
+
+        {/* Station, last updated and back button */}
+        <HeadingWidget
+          text={`Alerts and disruptions for ${toTitleCase(parsedStation)}`}
+          tag={`Last updated at ${now}`}
+        >
+          <Button width="w-full md:w-56" back>
+            <ArrowLeftIcon /> Back to alerts
+          </Button>
+        </HeadingWidget>
+      </section>
+
+      {/* Alerts */}
+      <section className="flex flex-col items-center gap-6">
+        <Suspense fallback={<Skeletons height="h-24" />}>
+          <AlertsSection station={parsedStation} />
+        </Suspense>
+      </section>
     </main>
   );
 }
---
+++ app/components/alert/AlertsSection.tsx
@@ -0,0 +1,22 @@
+import getAlerts from '@/app/utils/getAlerts';
+import Notice from '../notice/Notice';
+import { Alert } from '@/app/interfaces';
+import toTitleCase from '@/app/utils/toTitleCase';
+
+export default async function AlertsSection({ station }: { station: string }) {
+  const alerts = await getAlerts(station);
+
+  if (alerts && alerts.length > 0) {
+    return alerts.map((alert: Alert, index: number) => (
+      <Notice key={index} notice="Alert" description={alert.message} status="alert" />
+    ));
+  } else {
+    return (
+      <Notice
+        notice="No alerts"
+        description={`There are currently no alerts or disruptions affecting ${toTitleCase(station)}.`}
+        status="success"
+      />
+    );
+  }
+}
