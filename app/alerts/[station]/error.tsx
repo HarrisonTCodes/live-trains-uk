ec848d9f21cba347aac3dcc20b8bf0ad0a3d67e0
HarrisonTCodes <htillbackup@gmail.com>
2025-02-25 19:22:58 +0000

    Added back button to error pages and error page for alerts

--- app/alerts/[station]/error.tsx
+++ app/alerts/[station]/error.tsx
@@ -7,11 +7,11 @@ export default function Error() {
     <main className="flex flex-col items-center gap-6 py-8">
       <section className="relative w-[90vw] max-w-[700px]">
         <BackButton />
-        <h1 className="pb-2 text-center text-2xl font-bold text-blue-900">Service Details</h1>
+        <h1 className="text-center text-2xl font-bold text-blue-900">Alerts and Disruptions</h1>
       </section>
       <Notice
         notice="Error"
-        description="There was an error trying to retrieve the service. This service may no longer exist"
+        description="There was an error retrieving alerts for this station. Please make sure you choose a valid station from the dropdown menu."
         status="fail"
       />
     </main>
--- app/trains/[from]/[to]/error.tsx
+++ app/trains/[from]/[to]/error.tsx
@@ -1,10 +1,14 @@
 'use client';
+import BackButton from '@/app/components/button/BackButton';
 import Notice from '@/app/components/notice/Notice';
 
 export default function Error() {
   return (
     <main className="flex flex-col items-center gap-6 py-8">
-      <h1 className="text-center text-2xl font-bold text-blue-900">Live Departures</h1>
+      <section className="relative w-[90vw] max-w-[700px]">
+        <BackButton />
+        <h1 className="text-center text-2xl font-bold text-blue-900">Live Departures</h1>
+      </section>
       <Notice
         notice="Error"
         description="There was an error retrieving services between these stations. Please make sure you choose valid stations from the dropdown menu."
