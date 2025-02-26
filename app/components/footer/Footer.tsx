806c6c20b51ec6755dc928d5e277ba17c9c646ce
HarrisonTCodes <htillbackup@gmail.com>
2025-02-26 19:42:24 +0000

    Updated footer border colour for consistency

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import FooterLink from './FooterLink';
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col items-center justify-center gap-4 border-t bg-stone-100 py-4 md:flex-row">
+    <footer className="flex flex-col items-center justify-center gap-4 border-t border-gray-300 bg-stone-100 py-4 md:flex-row">
       <FooterLink href={'https://www.raildeliverygroup.com'}>
         Powered by Rail Delivery Group
       </FooterLink>
