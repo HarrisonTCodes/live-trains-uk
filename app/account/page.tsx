5fcb66a60d269eb9f5b1b25ec3bca9979bb6aeab
HarrisonTCodes <htillbackup@gmail.com>
2025-03-29 21:05:36 +0000

    Added initial content to account page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,11 +1,21 @@
 import Link from 'next/link';
 import Button from '../components/button/Button';
 import HeadingWidget from '../components/heading-widget/HeadingWidget';
-import { LogOutIcon } from 'lucide-react';
+import { LogOutIcon, Trash2Icon } from 'lucide-react';
 import { auth } from '@/auth';
+import prisma from '../utils/prisma';
 
 export default async function AccountPage() {
   const session = await auth();
+  const { accountCreatedAt, journeyCount } = await prisma.user
+    .findUnique({
+      where: { email: session!.user!.email! },
+      select: { createdAt: true, _count: { select: { journeys: true } } },
+    })
+    .then((response) => ({
+      accountCreatedAt: response!.createdAt,
+      journeyCount: response!._count.journeys,
+    }));
 
   return (
     <main className="flex flex-grow flex-col items-center gap-6 py-8">
@@ -13,9 +23,7 @@ export default async function AccountPage() {
         {/* Heading */}
         <h1 className="pb-2 text-center text-2xl font-bold text-blue-900">Account</h1>
 
-        <HeadingWidget
-          text={`Manage your Live Trains UK account (${session?.user?.email}) and its associated data`}
-        >
+        <HeadingWidget text={`Manage your Live Trains UK account and its associated data`}>
           <Link href={'/signout'} className="w-full whitespace-nowrap md:w-40">
             <Button className="w-full">
               <LogOutIcon /> Sign Out
@@ -23,6 +31,19 @@ export default async function AccountPage() {
           </Link>
         </HeadingWidget>
       </section>
+
+      <div className="flex w-[90vw] max-w-[700px] flex-col gap-4 rounded-lg border border-stone-300 bg-white p-2">
+        <h2>Account Details</h2>
+        <section>
+          <p>Email Address: {session?.user?.email}</p>
+          <p>Name: {session?.user?.name}</p>
+          <p>Date Created: {accountCreatedAt.toDateString()}</p>
+          <p>Number of Saved Journeys: {journeyCount}</p>
+        </section>
+        <Button variant="destructive" className="w-full">
+          <Trash2Icon /> Delete Account
+        </Button>
+      </div>
     </main>
   );
 }
