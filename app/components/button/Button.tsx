121efe641ea1128a9d35462d43a1a0c5c152fd0d
HarrisonTCodes <htillbackup@gmail.com>
2025-03-29 13:55:02 +0000

    Changed variant and status enum properties to be consistent

--- app/components/button/Button.tsx
+++ app/components/button/Button.tsx
@@ -22,7 +22,7 @@ export default function Button({
 
   return (
     <button
-      className={`flex items-center justify-center gap-2 rounded-lg p-2 font-medium transition disabled:cursor-not-allowed ${variant == 'secondary' && 'border border-stone-300 enabled:hover:bg-stone-100 disabled:bg-stone-200 disabled:text-stone-400'} ${variant == 'primary' && 'border border-blue-800 bg-blue-800 text-white enabled:hover:border-blue-600 enabled:hover:bg-blue-600'} ${variant == 'destructive' && 'border border-red-700 bg-red-700 text-white enabled:hover:border-red-600 enabled:hover:bg-red-600'} ${className}`}
+      className={`flex items-center justify-center gap-2 rounded-lg p-2 font-medium transition disabled:cursor-not-allowed ${variant === 'secondary' && 'border border-stone-300 enabled:hover:bg-stone-100 disabled:bg-stone-200 disabled:text-stone-400'} ${variant === 'primary' && 'border border-blue-800 bg-blue-800 text-white enabled:hover:border-blue-600 enabled:hover:bg-blue-600'} ${variant === 'destructive' && 'border border-red-700 bg-red-700 text-white enabled:hover:border-red-600 enabled:hover:bg-red-600'} ${className}`}
       // If button has no onclick and is a back button, onclick navigate to previous page
       onClick={onClick ?? (back ? () => router.back() : () => {})}
       type={submit ? 'submit' : 'button'}
--- app/components/notice/Notice.tsx
+++ app/components/notice/Notice.tsx
@@ -4,19 +4,19 @@ export default function Notice({
   notice,
   description,
   className,
-  status,
+  status = 'info',
 }: {
   notice: string;
   description: string;
   className?: string;
-  status?: 'success' | 'fail' | 'alert' | 'warning';
+  status?: 'info' | 'success' | 'fail' | 'alert' | 'warning';
 }) {
   return (
     <div
-      className={`min-h-24 w-[90vw] max-w-[700px] gap-2 rounded-lg border p-2 ${!status && 'border-stone-300 bg-white'} ${status === 'success' && 'border-green-700 bg-green-50 text-green-900'} ${(status === 'fail' || status === 'alert') && 'border-red-700 bg-red-50 text-red-900'} ${status === 'warning' && 'border-orange-700 bg-orange-50 text-orange-900'} ${className}`}
+      className={`min-h-24 w-[90vw] max-w-[700px] gap-2 rounded-lg border p-2 ${status === 'info' && 'border-stone-300 bg-white'} ${status === 'success' && 'border-green-700 bg-green-50 text-green-900'} ${(status === 'fail' || status === 'alert') && 'border-red-700 bg-red-50 text-red-900'} ${status === 'warning' && 'border-orange-700 bg-orange-50 text-orange-900'} ${className}`}
     >
       <h2 className="flex items-center gap-2 text-xl font-medium">
-        {!status && <InfoIcon className="text-blue-800" />}
+        {status === 'info' && <InfoIcon className="text-blue-800" />}
         {status === 'success' && <CircleCheckIcon />}
         {status === 'fail' && <TriangleAlertIcon />}
         {(status === 'alert' || status === 'warning') && <CircleAlertIcon />}
