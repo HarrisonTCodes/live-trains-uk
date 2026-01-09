8b6c5c4aecd2fc8a699be2297b398800ca69904c
HarrisonTCodes <htillbackup@gmail.com>
2026-01-09 17:17:42 +0000

    Added improved error observability in get alerts utility

--- app/utils/getAlerts.ts
+++ app/utils/getAlerts.ts
@@ -1,4 +1,4 @@
-import { AlertResponse, DisruptionResponse } from '../interfaces';
+import { DisruptionResponse } from '../interfaces';
 import unescape from 'lodash.unescape';
 import { stations } from './stations';
 
@@ -16,10 +16,31 @@ export default async function getAlerts(station: string) {
   headers.set('x-apikey', process.env.DISRUPTIONS_API_KEY!);
 
   // Get alerts
-  const response = (await fetch(
+  const response = await fetch(
     `https://api1.raildata.org.uk/1010-disruption-list1_0/LDBSVWS/api/20220120/GetDisruptionList/${stationCrs}`,
     { headers, cache: 'no-store' },
-  ).then((response) => response.json())) as AlertResponse[];
+  )
+    .then((response) => response.json())
+    .catch((err) => {
+      console.error({
+        event: 'error_getting_alerts',
+        stationCrs,
+        error: err,
+      });
+
+      throw err;
+    });
+
+  // If the response has a fault attribute, something has necessarily gone fatally wrong
+  if (response.fault) {
+    console.error({
+      event: 'fault_getting_alerts',
+      stationCrs,
+      fault: response.fault,
+    });
+
+    throw Error('Failed to access disruptions API');
+  }
 
   return response[0].disruptions.map((disruption: DisruptionResponse) => ({
     message: unescape(
