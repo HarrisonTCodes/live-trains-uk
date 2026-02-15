76a707094707e335e97f7e666af8e668f8453d48
HarrisonTCodes <htillbackup@gmail.com>
2026-02-15 13:24:16 +0000

    Refactored tag component to use object indexing instead of conditionals for status styling

--- app/components/tag/Tag.tsx
+++ app/components/tag/Tag.tsx
@@ -1,3 +1,9 @@
+const statusStyles = {
+  neutral: 'border-stone-300 bg-white',
+  success: 'border-green-700 bg-green-50 text-green-900',
+  fail: 'border-red-700 bg-red-50 text-red-900',
+};
+
 export default function Tag({
   children,
   status = 'neutral',
@@ -7,7 +13,7 @@ export default function Tag({
 }) {
   return (
     <div
-      className={`flex h-8 w-fit items-center gap-1 whitespace-nowrap rounded-full border px-2 py-1 text-sm font-medium ${status === 'neutral' && 'border-stone-300 bg-white'} ${status === 'success' && 'border-green-700 bg-green-50 text-green-900'} ${status === 'fail' && 'border-red-700 bg-red-50 text-red-900'}`}
+      className={`flex h-8 w-fit items-center gap-1 whitespace-nowrap rounded-full border px-2 py-1 text-sm font-medium ${statusStyles[status]}`}
     >
       {children}
     </div>
