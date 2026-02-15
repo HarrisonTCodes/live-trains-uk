e263d0db18531a695972105f1a6f3e75b24052ab
HarrisonTCodes <htillbackup@gmail.com>
2026-02-15 13:24:37 +0000

    Refactored notice component to use object indexing instead of conditionals for status styling and icons

--- app/components/notice/Notice.tsx
+++ app/components/notice/Notice.tsx
@@ -1,5 +1,28 @@
 import { InfoIcon, CircleCheckIcon, CircleAlertIcon, TriangleAlertIcon } from 'lucide-react';
 
+const statusDetails = {
+  info: {
+    style: 'border-stone-300 bg-white',
+    icon: <InfoIcon className="text-blue-800" />,
+  },
+  success: {
+    style: 'border-green-700 bg-green-50 text-green-900',
+    icon: <CircleCheckIcon />,
+  },
+  fail: {
+    style: 'border-red-700 bg-red-50 text-red-900',
+    icon: <TriangleAlertIcon />,
+  },
+  alert: {
+    style: 'border-red-700 bg-red-50 text-red-900',
+    icon: <CircleAlertIcon />,
+  },
+  warning: {
+    style: 'border-orange-700 bg-orange-50 text-orange-900',
+    icon: <CircleAlertIcon />,
+  },
+};
+
 export default function Notice({
   notice,
   description,
@@ -13,13 +36,10 @@ export default function Notice({
 }) {
   return (
     <div
-      className={`min-h-24 w-[90vw] max-w-[700px] gap-2 rounded-lg border p-2 ${status === 'info' && 'border-stone-300 bg-white'} ${status === 'success' && 'border-green-700 bg-green-50 text-green-900'} ${(status === 'fail' || status === 'alert') && 'border-red-700 bg-red-50 text-red-900'} ${status === 'warning' && 'border-orange-700 bg-orange-50 text-orange-900'} ${className}`}
+      className={`min-h-24 w-[90vw] max-w-[700px] gap-2 rounded-lg border p-2 ${statusDetails[status].style} ${className}`}
     >
       <h2 className="flex items-center gap-2 text-xl font-medium">
-        {status === 'info' && <InfoIcon className="text-blue-800" />}
-        {status === 'success' && <CircleCheckIcon />}
-        {status === 'fail' && <TriangleAlertIcon />}
-        {(status === 'alert' || status === 'warning') && <CircleAlertIcon />}
+        {statusDetails[status].icon}
         {notice}
       </h2>
       <p>{description}</p>
