9b75a3622ed1c92e6e19cea928fcd89270944c41
HarrisonTCodes <htillbackup@gmail.com>
2026-05-10 15:57:21 +0100

    Added Buy Me a Coffee footer link

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -3,12 +3,13 @@ import FooterLink from './FooterLink';
 export default function Footer() {
   return (
     <footer className="flex flex-col items-center justify-center gap-4 border-t border-gray-300 bg-stone-100 py-4 md:flex-row">
-      <FooterLink href={'https://www.raildeliverygroup.com'}>
+      <FooterLink href="https://www.raildeliverygroup.com">
         Powered by Rail Delivery Group
       </FooterLink>
-      <FooterLink href={'https://github.com/HarrisonTCodes/live-trains-uk'}>
+      <FooterLink href="https://github.com/HarrisonTCodes/live-trains-uk">
         View the Source Code
       </FooterLink>
+      <FooterLink href="https://buymeacoffee.com/harrisontcodes">Buy Me a Coffee</FooterLink>
     </footer>
   );
 }
