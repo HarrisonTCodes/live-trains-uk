a011802adad7c77f77ed838287f309b453f26269
HarrisonTCodes <htillbackup@gmail.com>
2026-05-11 19:02:54 +0100

    Added initial step-by-step plan UI using calling point components

--- app/components/plan-info/LegInfo.tsx
+++ app/components/plan-info/LegInfo.tsx
@@ -2,39 +2,71 @@ import { Leg } from '@/app/interfaces';
 import formatDate from '@/app/utils/formatDate';
 import toTitleCase from '@/app/utils/toTitleCase';
 import { TrainFrontIcon, TrainFrontTunnelIcon } from 'lucide-react';
+import CallingPointGraphic from '../calling-point-info/CallingPointGraphic';
 
-export default function LegInfo({ leg }: { leg: Leg }) {
+export default function LegInfo({
+  leg,
+  isFirstLeg,
+  isLastLeg,
+}: {
+  leg: Leg;
+  isFirstLeg: boolean;
+  isLastLeg: boolean;
+}) {
   const departureTimeString = formatDate(leg.departure.time);
   const arrivalTimeString = formatDate(leg.arrival.time);
 
   return (
-    <section className="p-2">
-      {/* Mode */}
-      <h2 className="flex items-center gap-1 text-lg text-stone-600">
-        {leg.mode === 'train' ? (
-          <>
-            <TrainFrontIcon /> Train
-          </>
-        ) : (
-          <>
-            <TrainFrontTunnelIcon /> Tube{' '}
-            {leg.undergroundInfo
-              ? `(${leg.undergroundInfo.line} Line, ${leg.undergroundInfo.direction})`
-              : ''}
-          </>
-        )}
-      </h2>
+    <>
+      <PlanStationInfo
+        station={leg.departure.station}
+        time={formatDate(leg.departure.time)}
+        isFirstStation={isFirstLeg}
+      />
+      {isLastLeg && (
+        <PlanStationInfo
+          station={leg.arrival.station}
+          time={formatDate(leg.arrival.time)}
+          isLastStation={isLastLeg}
+        />
+      )}
+    </>
+  );
+}
 
-      {/* Departure */}
-      <section className="flex justify-between">
-        <p className="text-lg font-medium">{toTitleCase(leg.departure.station)}</p>
-        <p className="text-lg font-medium">{departureTimeString}</p>
-      </section>
+function PlanStationInfo({
+  station,
+  time,
+  isFirstStation,
+  isLastStation,
+}: {
+  station: string;
+  time: string;
+  isFirstStation?: boolean;
+  isLastStation?: boolean;
+}) {
+  return (
+    <section className="flex items-center gap-2">
+      {/* Graphic */}
+      {/* TODO: GENERALISE THIS */}
+      <CallingPointGraphic
+        isFirstPoint={isFirstStation}
+        isLastPoint={isLastStation}
+        departed={false}
+      />
 
-      {/* Arrival */}
-      <section className="flex justify-between">
-        <p className="text-lg font-medium">{toTitleCase(leg.arrival.station)}</p>
-        <p className="text-lg font-medium">{arrivalTimeString}</p>
+      {/* Details */}
+      <section className="flex h-24 w-full justify-between gap-1 pr-2 pt-9">
+        {/* Station and platform */}
+        <div className="flex flex-col gap-1">
+          <p className={`pl-1 ${(isFirstStation || isLastStation) && 'font-bold'}`}>{station}</p>
+          {/* {callingPoint.platform && <Tag>Platform {callingPoint.platform}</Tag>} */}
+        </div>
+
+        {/* Times */}
+        <div className="flex flex-col items-end gap-1">
+          <p className={`pr-1 ${(isFirstStation || isLastStation) && 'font-bold'}`}>{time}</p>
+        </div>
       </section>
     </section>
   );
--- app/components/plan-info/PlanInfo.tsx
+++ app/components/plan-info/PlanInfo.tsx
@@ -2,12 +2,15 @@ import { Leg } from '@/app/interfaces';
 import LegInfo from './LegInfo';
 
 export default function PlanInfo({ plan }: { plan: Leg[] }) {
+  console.log(plan);
   return (
-    <div className="flex w-[90vw] max-w-[700px] flex-col gap-2 divide-y divide-gray-300 rounded-lg border border-gray-300 bg-white">
-      {plan.map((leg: Leg) => (
+    <div className="relative flex w-[90vw] max-w-[700px] flex-col rounded-lg border border-gray-300 bg-white px-2 pb-4">
+      {plan.map((leg: Leg, index: number) => (
         <LegInfo
           key={`${leg.departure.crs} ${leg.departure.time} to ${leg.arrival.crs} ${leg.arrival.time}`}
           leg={leg}
+          isFirstLeg={index === 0}
+          isLastLeg={index === plan.length - 1}
         />
       ))}
     </div>
