bfcd448076c09837f1debe0784c89ca09e29bb49
HarrisonTCodes <htillbackup@gmail.com>
2026-05-17 10:39:07 +0100

    Added standalone get ICS ID method

---
+++ app/utils/getIcsId.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { StopPointsResponse } from '../interfaces';
+
+export default async function getIcsId(station: string): Promise<string> {
+  const response = (await axios
+    .get(process.env.STOP_POINT_SEARCH_BASE_URL!, {
+      params: {
+        query: station,
+        app_key: process.env.STOP_POINT_SEARCH_API_KEY,
+      },
+    })
+    .then((response) => response.data)) as StopPointsResponse;
+
+  // Get result that has every word in name
+  const nameWords = station.toLowerCase().split(' ');
+  let ics = response.matches.find((match) => {
+    const matchWords = new Set(match.name.toLowerCase().split(' '));
+    return nameWords.every((word: string) => matchWords.has(word));
+  })?.icsId;
+
+  // If no word-match found, fallback to first result
+  if (!ics) {
+    ics = response.matches[0].icsId;
+  }
+
+  return ics;
+}
