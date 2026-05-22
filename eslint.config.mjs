388165fe542997db4deaca95cf2523f76ae7bd1e
HarrisonTCodes <htillbackup@gmail.com>
2026-05-22 16:41:13 +0100

    Added prisma generated folder to eslint ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import nextVitals from 'eslint-config-next/core-web-vitals';
 
 const eslintConfig = [
   {
-    ignores: ['.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+    ignores: ['.next/**', 'out/**', 'build/**', 'next-env.d.ts', 'app/utils/prisma/client/**'],
   },
   ...nextVitals,
   {
