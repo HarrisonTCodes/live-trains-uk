0e2a3c6e67b1348b1130946bb86748b02dbf477a
HarrisonTCodes <htillbackup@gmail.com>
2026-06-05 16:29:09 +0100

    Added main neon branch to terraform

--- infra/db.tf
+++ infra/db.tf
@@ -1,3 +1,8 @@
 resource "neon_project" "live_trains_uk_db" {
   name = "live-trains-uk-db"
 }
+
+resource "neon_branch" "main" {
+  project_id = neon_project.live_trains_uk_db.id
+  name       = "main"
+}
