cfa822e48dd174d600ad7df241f3b69a533fc324
HarrisonTCodes <htillbackup@gmail.com>
2026-06-05 16:31:49 +0100

    Added default neon role to terraform

--- infra/db.tf
+++ infra/db.tf
@@ -6,3 +6,9 @@ resource "neon_branch" "main" {
   project_id = neon_project.live_trains_uk_db.id
   name       = "main"
 }
+
+resource "neon_role" "default" {
+  project_id = neon_project.live_trains_uk_db.id
+  branch_id  = neon_branch.main.id
+  name       = "default"
+}
