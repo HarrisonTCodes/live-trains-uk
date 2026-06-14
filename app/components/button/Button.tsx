202f33d0d0c2a06d7598180acc46c863dbb95879
HarrisonTCodes <htillbackup@gmail.com>
2026-06-14 14:43:57 +0100

    Changed button variant styling from ternary to map

--- app/components/button/Button.tsx
+++ app/components/button/Button.tsx
@@ -1,6 +1,15 @@
 'use client';
 import { useRouter } from 'next/navigation';
 
+const variantStyles = {
+  primary:
+    'border border-blue-800 bg-blue-800 text-white enabled:hover:border-blue-600 enabled:hover:bg-blue-600',
+  secondary:
+    'border border-stone-300 enabled:hover:bg-stone-100 disabled:bg-stone-200 disabled:text-stone-400',
+  destructive:
+    'border border-red-700 bg-red-700 text-white enabled:hover:border-red-600 enabled:hover:bg-red-600',
+};
+
 export default function Button({
   children,
   onClick,
@@ -22,7 +31,7 @@ export default function Button({
 
   return (
     <button
-      className={`flex items-center justify-center gap-2 rounded-lg p-2 font-medium transition disabled:cursor-not-allowed ${variant === 'secondary' && 'border border-stone-300 enabled:hover:bg-stone-100 disabled:bg-stone-200 disabled:text-stone-400'} ${variant === 'primary' && 'border border-blue-800 bg-blue-800 text-white enabled:hover:border-blue-600 enabled:hover:bg-blue-600'} ${variant === 'destructive' && 'border border-red-700 bg-red-700 text-white enabled:hover:border-red-600 enabled:hover:bg-red-600'} ${className}`}
+      className={`flex items-center justify-center gap-2 rounded-lg p-2 font-medium transition disabled:cursor-not-allowed ${variantStyles[variant]} ${className}`}
       // If button has no onclick and is a back button, onclick navigate to previous page
       onClick={onClick ?? (back ? () => router.back() : () => {})}
       type={submit ? 'submit' : 'button'}
